36ee81c2a6744dc7a1c33120be53bef8e865012a
Ainur Otega <ainuralimjan@Mac.lan>
2025-04-21 16:27:04 -0700

    first step

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+// create empty java project
+// create repo in github
+// push your project there
+// create two branches from main
+// merge those two branches into main
+
+
+
+}
