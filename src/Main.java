e2c381e37ddd3d3db25927347d9345960fbbf4cc
Ainur Otega <ainuralimjan@Mac.lan>
2025-04-21 22:28:44 -0700

    add more prints

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
+        System.out.println("Hello guys");
     }
-// create empty java project
-// create repo in github
-// push your project there
-// create two branches from main
-// merge those two branches into main
-
-
-
 }
